e388737303e28ba5d484ecd6c054ed342588e1c5
Nelson Ramirez <xhyskoax@gmail.com>
2024-05-13 11:48:52 -0400

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: "/m6-s2-desafio-opcional/",
+  outputDir: "docs"
 })
